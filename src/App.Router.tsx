4a9b5b79ca41db8470f645ceb150022736cdc714
Elias <thewillthatishiddeninside@gmail.com>
2023-05-20 15:09:40 +0300

    Fix

--- src/App.Router.tsx
+++ src/App.Router.tsx
@@ -1,21 +1,19 @@
-import { createBrowserRouter, Navigate, RouteObject } from 'react-router-dom';
+import { createHashRouter, Navigate, RouteObject } from 'react-router-dom';
 import ResponsiveTable from './pages/responsive-table/ResponsiveTable.tsx';
 
 const routes: RouteObject[] = [
-    {
-      path: '/', children: [
-        { path: '/responsive-table', element: <ResponsiveTable/> },
-        { path: '/test', element: <h1>TEST</h1> },
-      ]
-    },
-    { path: '*', element: <Navigate to="/responsive-table" replace/> },
-    { path: '', element: <Navigate to="/responsive-table" replace/> }
-  ]
-;
+  {
+    path: '/', children: [
+      { path: '/responsive-table', element: <ResponsiveTable/> },
+      { path: '/test', element: <h1>TEST</h1> },
+      { path: '*', element: <Navigate to="/responsive-table" replace/> },
+      { path: '', element: <Navigate to="/responsive-table" replace/> }
+    ]
+  },
+  { path: '*', element: <Navigate to="/" replace/> },
+  { path: '', element: <Navigate to="/" replace/> }
+];
 
-const appRouter = createBrowserRouter(routes, {
-  basename: import.meta.env.VITE_BASE_URL
-});
-
+const appRouter = createHashRouter(routes, {});
 
 export default appRouter;
